1ed9a558723d707ef3ee81007ca8c27ca1ed2e52
Ntare1 <isaacntare1@gmail.com>
2022-11-25 08:14:11 +0200

    prinf

--- _printf.c
+++ _printf.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
+
 /**
- * _printf - Prints input to output
- * @format: format
- * Return: number of printed characters
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
@@ -15,6 +16,7 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
+
 	va_start(list, format);
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
@@ -34,14 +36,17 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-					flags, width, precision, size);
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
 		}
 	}
+
 	print_buffer(buffer, &buff_ind);
+
 	va_end(list);
+
 	return (printed_chars);
 }
 
@@ -54,5 +59,6 @@ void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
+
 	*buff_ind = 0;
 }
