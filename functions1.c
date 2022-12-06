984f188300f89fa9458c0e16beac37f59abd2a4e
Jovz19200 <gisubizo.jovan@gmail.com>
2022-12-06 09:32:27 -0800

    function1 file

--- functions1.c
+++ functions1.c
@@ -5,9 +5,9 @@
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: get width
+ * @width: get width (output)
  * @precision: Precision specification
- * @size: Size specifier
+ * @size: Size specifier (input)
  * Return: Number of chars printed.
  */
 int print_unsigned(va_list types, char buffer[],
